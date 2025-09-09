54d8312c3b375a671713d30248cff183842f9f1f
Jasper Kroon <jasper021>
2025-09-09 17:56:16 +0200

    commentaar toegevoegd

--- Quest.cs
+++ Quest.cs
@@ -1,4 +1,11 @@
 public static class Quest
 {
-    
+    //As a player I want to start a quest so I can finish it (in order to complete the game)
+
+    //When the player enters a location with a quest, ask the user if he wants to start the quest or not
+    //If the player does not want to start the quest, then the user can choose to go to another location instead of the quest location
+    //If the player starts the quest then the player gets a message that the quest has started with the objective
+    //The player gets to see what the quest is about and what rewards are for the quest
+
+
 }
