20938f2e4e330dd69de0cc44165f18a048af7690
Daniel Nyxraze <1094235@hr.nl>
2025-09-09 16:03:50 +0200

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,4 +1,16 @@
 public static class Player
 {
-    
+    public List<string> QuestCompleted { get; set; } = new List<string>();
+    public string location { get; set; }
+    public List<string> inventory { get; set; } = new List<string>();
+
+    public bool PlayerHasWon()
+    {
+        bool DoneThreeQuest = QuestCompleted.Count >= 3;
+        bool AtCastle = location == "Castle";
+        bool hasGoldenSpider = inventory.Contains("Golden Spider");
+
+        return DoneThreeQuest && AtCastle && hasGoldenSpider;
+    }
+
 }
