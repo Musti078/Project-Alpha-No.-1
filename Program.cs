10325513149e15cd18c0cfea8826985b9c9b334c
Daniel Nyxraze <1094235@hr.nl>
2025-09-15 13:41:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
-public class Progran
+public class Program
 {
     public static void Main()
     {
+        Location locations = new Location(locations, username, _);
         
+
+        Console.WriteLine("Enter your name warrior");
+        string userName = Console.ReadLine() ?? "";
+
+
+        Console.WriteLine(Locations[0]);
+        Console.WriteLine("Where would you like to go?");
+
+
     }
+
+
 }
