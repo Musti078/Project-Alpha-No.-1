94679b25cb79fe5f29f25b7cf1ed8bfa74d69c34
Daniel Nyxraze <1094235@hr.nl>
2025-09-15 13:52:40 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,14 @@ public class Program
     public static void Main()
     {
         Location locations = new Location(locations, username, _);
-        
+        Player player = new Player(100, 100);
+        Weapon weapon = new Weapon(userName, 5);
+        Monster Rat = new Monster(100, rat, 6, 20, Splinter);
+        Monster Snake = new Monster(100, snake, 10,  50, Snakey);
+        Monster GiantSpider = new Monster(100, giantSpider, 15, 100, Spidey);
+        Quest quests = new Quest();
+
+
 
         Console.WriteLine("Enter your name warrior");
         string userName = Console.ReadLine() ?? "";
@@ -11,6 +18,8 @@ public class Program
 
         Console.WriteLine(Locations[0]);
         Console.WriteLine("Where would you like to go?");
+        string usermovement = Console.ReadLine() ?? "";
+
 
 
     }
