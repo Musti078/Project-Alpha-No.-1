b46f49400949ce194856741add973e2330e2c352
Daniel Nyxraze <1094235@hr.nl>
2025-09-16 12:00:55 +0200

    Update Location.cs

--- Location.cs
+++ Location.cs
@@ -4,7 +4,7 @@ public class Location
     public int ID;
     public string Name;
     public string Description;
-    public Item ItemRequiredToEnter;
+    public bool ItemRequiredToEnter;
     public Quest QuestAvailableHere;
     public Monster MonsterLivingHere;
     public Location LocationToNorth;
@@ -13,7 +13,7 @@ public class Location
     public Location LocationToWest;
 
     // constructor with the parameters and assigning them correctly
-    public Location(int id, string name, string description, Item itemRequired = null, Quest questready = null, Monster monsterHere = null)
+    public Location(int id, string name, string description, bool itemRequired, Quest questready = null, Monster monsterHere = null)
     {
         ID = id;
         Name = name;
